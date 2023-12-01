00a5834a5726019adf4d56bd2a14519102b7f3b2
yamada029 <syouta.y0829@gmail.com>
2023-12-01 16:29:51 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,23 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Header from '..components/header.js'
+import Hero from '..components/hero.js'
+import Footer from '..components/footer.js'
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export default function Home() {
   return(
     <>
-      Home 
+    <header>HEADER</header>
+    
+    <main>
+      <div>
+        <h1>CUBE</h1>
+        <p>アウトプットしていくサイト</p>
+      </div>
+    </main>
+
+    <footer>FOOTER</footer>
     </>
   )
 }
